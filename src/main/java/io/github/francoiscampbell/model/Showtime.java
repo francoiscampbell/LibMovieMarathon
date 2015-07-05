780283aaf439326d40b229eb9ea4af7d00cbb6da
Francois Campbell <campbell.francois@gmaill.com>
2015-07-05 12:07:18 -0400

    Added Schedule class. Added all API parameters to interface.

--- src/main/java/io/github/francoiscampbell/model/Showtime.java
+++ src/main/java/io/github/francoiscampbell/model/Showtime.java
@@ -2,17 +2,17 @@ package io.github.francoiscampbell.model;
 
 import io.github.francoiscampbell.apimodel.ApiShowtime;
 import org.jetbrains.annotations.NotNull;
-import org.joda.time.LocalDateTime;
+import org.joda.time.DateTime;
 
 /**
  * Created by francois on 15-07-03.
  */
 public class Showtime implements Comparable<Showtime> {
-    private final LocalDateTime dateTime;
+    private final DateTime dateTime;
     private final Movie movie;
 
     public Showtime(String dateTime, Movie movie) {
-        this.dateTime = LocalDateTime.parse(dateTime);
+        this.dateTime = DateTime.parse(dateTime);
         this.movie = movie;
     }
 
@@ -24,22 +24,35 @@ public class Showtime implements Comparable<Showtime> {
         return movie;
     }
 
-    public LocalDateTime getDateTime() {
+    public DateTime getStartDateTime() {
         return dateTime;
     }
 
-    public String getTime() {
+    public DateTime getEndDateTime() {
+        return dateTime.plus(getMovie().getTotalLength());
+    }
+
+    public String getStartTimeString() {
         return String.format("%02d:%02d", dateTime.getHourOfDay(), dateTime.getMinuteOfHour());
+    }
+
+    public String getEndTimeString() {
+        DateTime endTime = getEndDateTime();
+        return String.format("%02d:%02d", endTime.getHourOfDay(), endTime.getMinuteOfHour());
     }
 
     @Override
     public int compareTo(@NotNull Showtime o) {
-        return dateTime.compareTo(o.getDateTime());
+        return dateTime.compareTo(o.getStartDateTime());
     }
 
     @Override
     public String toString() {
-        return dateTime.toString() + " " + movie.toString();
+        return dateTime.toString() + "-" + getEndDateTime().toString() + " " + getMovie().toString();
+    }
+
+    public String toFriendlyString() {
+        return getStartTimeString() + "-" + getEndTimeString() + " " + getMovie().toString();
     }
 
 }
